5e7a4c677df93cc4bd6a0e1386f6c18ad11d4246
LongDefeat <masonsams7@gmail.com>
2023-08-18 19:03:35 -0700

    add spinning world and navbar and image

--- portfolio/src/app/portfolio/page.js
+++ portfolio/src/app/portfolio/page.js
@@ -1,13 +1,31 @@
+// components/Portfolio.js
 import Image from 'next/image';
 import projects from '@/data/projects';
 import ProjectCard from '@/components/ProjectCard';
 import styles from '@/styles/portfolio.module.css';
+import worldStyles from '@/styles/spinningWorld.module.css';
 
 const Portfolio = () => (
   <div className={styles.container}>
-    <main>
+    <main className={styles.main}>
       <h1 className={styles.title}>Mason Sams</h1>
-      <Image src={'/images/plaid-shirt.jpg'} alt="me" width={300} height={300} style={{ objectFit: 'contain' }} />
+      <div className={styles.infoContainer}>
+        <Image
+          className={styles.image}
+          src={'/images/plaid-shirt.jpg'}
+          alt="me"
+          width={300}
+          height={300}
+        />
+        <div className={styles.bio}>
+          <h2>About Me</h2>
+          <p>
+            I am a passionate developer seeking to create technologies that enhance the lives of the 
+            <span className={worldStyles.world}> 🌎 </span>
+            around him
+          </p>
+        </div>
+      </div>
       <div className={styles.projects}>
         {projects.map((project, index) => (
           <ProjectCard key={index} {...project} />
