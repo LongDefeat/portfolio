85233616f4510106d89706a2669fe870bc8706fd
Mason Sams <78603508+LongDefeat@users.noreply.github.com>
2024-01-20 21:29:48 -0800

    reconfigure code to deploy to vercel

--- portfolio/src/app/page.js
+++ portfolio/src/app/page.js
@@ -1,11 +1,11 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import projectsData from '@/data/projects';
-import Projects from './projects/page';
+import Link from "next/link";
+import Image from "next/image";
+import projectsData from "@/data/projects";
+import Projects from "./projects/page";
 
 /* Styles */
-import styles from '@/styles/portfolio.module.css';
-import computerStyles from '@/styles/spinningComputer.module.css';
+import styles from "@/styles/portfolio.module.css";
+import computerStyles from "@/styles/spinningComputer.module.css";
 
 const Portfolio = () => (
   <div className={styles.container}>
@@ -16,19 +16,26 @@ const Portfolio = () => (
       <div className={styles.infoContainer}>
         <Image
           className={styles.image}
-          src={'/images/plaid-shirt.jpg'}
+          src={"/images/plaid-shirt.jpg"}
           alt="me"
           width={300}
           height={300}
         />
         <div className={styles.bio}>
-          <h2>About Me <span className={computerStyles.computerSway}> 💻 </span></h2>
+          <h2>
+            About Me <span className={computerStyles.computerSway}> 💻 </span>
+          </h2>
           <p>
-            Hello! I'm Mason, a passionate developer seeking to create technologies that enhance the lives of those around me. Distinctive journey transitioning from law to dynamic software engineering. My goal is always seeking the intersection between problem-solving and creative expression.
+            Hello! I&aposm Mason, a passionate developer seeking to create
+            technologies that enhance the lives of those around me. Distinctive
+            journey transitioning from law to dynamic software engineering. My
+            goal is always seeking the intersection between problem-solving and
+            creative expression.
           </p>
         </div>
-        <h2 className={styles.featuredProjects}>Featured Projects</h2>
-          <Projects projects={projectsData} />
+        <h2>Featured Projects</h2>
+
+        <Projects projects={projectsData} />
       </div>
     </main>
   </div>
