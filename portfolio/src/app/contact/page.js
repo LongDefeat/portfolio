558c3cfd68a6d84912afbcb955826736eefbc709
Mason Sams <78603508+LongDefeat@users.noreply.github.com>
2024-01-20 21:25:16 -0800

    reconfigure code to deploy to vercel

--- portfolio/src/app/contact/page.js
+++ portfolio/src/app/contact/page.js
@@ -1,5 +1,5 @@
-import Image from 'next/image';
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 
 /* Styles */
 import styles from "@/styles/Contact.module.css";
@@ -16,28 +16,49 @@ const Contact = () => (
           height={300}
         />
         <div className={styles.bio}>
-          <h2 className={styles.contactHeader}>Let's Connect!</h2>
+          <h2 className={styles.contactHeader}>Let&aposs Connect!</h2>
           <p>
-            Whether you're interested in working with me or just want to say hello, I'd love to hear from you! 
-            Feel free to reach out using any of the methods below.
+            Whether you&aposre interested in working with me or just want to say
+            hello, I would love to hear from you! Feel free to reach out using
+            any of the methods below.
           </p>
-          
+
           <ul className={styles.contactList}>
             <li>
-              <strong className={styles.list}>Email:</strong> <a href="mailto:mason@example.com">masonsams7@gmail.com</a>
+              <strong className={styles.list}>Email:</strong>{" "}
+              <a href="mailto:masonsams7@gmail.com">masonsams7@gmail.com</a>
             </li>
             <li>
-              <strong className={styles.list}>LinkedIn:</strong> <a href="https://www.linkedin.com/in/masonsams/" target="_blank" rel="noopener noreferrer">linkedin.com/in/masonsams7</a>
+              <strong className={styles.list}>LinkedIn:</strong>{" "}
+              <a
+                href="https://www.linkedin.com/in/masonsams/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                linkedin.com/in/masonsams7
+              </a>
             </li>
             <li>
-              <strong className={styles.list}>GitHub:</strong> <a href="https://github.com/masonsams" target="_blank" rel="noopener noreferrer">https://github.com/LongDefeat</a>
+              <strong className={styles.list}>GitHub:</strong>{" "}
+              <a
+                href="https://github.com/masonsams"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                https://github.com/LongDefeat
+              </a>
             </li>
           </ul>
-          
+
           <p className={styles.note}>
-            Note: Please allow a couple of days for responses. I appreciate your patience!
+            Note: Please allow a couple of days for responses. I appreciate your
+            patience!
           </p>
-          <a href="/resume.pdf" download="Mason_Sams_Resume.pdf" className={styles.resumeLink}>
+          <a
+            href="/resume.pdf"
+            download="Mason_Sams_Resume.pdf"
+            className={styles.resumeLink}
+          >
             Download My Resume
           </a>
         </div>
